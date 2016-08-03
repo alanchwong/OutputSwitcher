6e4e3fecc091b566936dff4c4626e8377ca3b178
Alan Wong <flyingwok@gmail.com>
2016-08-02 22:10:47 -0700

    Oops, forgot to include a file in last commit to add the "None" item to preset drop down menus.

---
+++ OutputSwitcherTray/NoPresetPresetContextMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutputSwitcher.TrayApp
+{
+    internal class NoPresetPresetContextMenuItem : ToolStripButton
+    {
+        public NoPresetPresetContextMenuItem()
+        {
+            Text = "None";
+            Enabled = false;
+        }
+    }
+}
